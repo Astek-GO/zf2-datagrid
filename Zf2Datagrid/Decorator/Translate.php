db3eaf8a010a42342411fe257292c8a0d8b14d7c
Thomas Sieffert <caporaldead@gmail.com>
2015-05-28 11:46:34 +0200

    Ajout d'un décorateur Translate

---
+++ Zf2Datagrid/Decorator/Translate.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Zf2Datagrid\Decorator;
+
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zf2Datagrid\RowAware;
+use Zf2Datagrid\RowAwareInterface;
+use Zf2Datagrid\ServiceLocatorAware;
+use Zf2Datagrid\Translator;
+
+/**
+ * Class Translate
+ *
+ * @package Zf2Datagrid\Decorator
+ */
+class Translate implements ServiceLocatorAwareInterface, RowAwareInterface
+{
+    use ServiceLocatorAware, RowAware, Translator;
+
+    /**
+     * @param $data
+     *
+     * @return string
+     */
+    public function render($data)
+    {
+        return $this->getTranslator()->translate($data);
+    }
+}
